fed2227c7cb8f22b14f0e7f412fd06148c2d68ed
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-03-24 16:01:42 +0530

    Adding home page

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/dashboard";
 
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import Login from "../pages/login";
 import Signup from "../pages/signup";
 import Privateroute from "./privateroute";
@@ -10,31 +10,46 @@ const Navbar = () => {
   return (
     <>
       <Box
-        h="50px"
+        // h="50px"
         display="flex"
         justifyItems="center"
         alignItems="center"
         fontWeight="bold"
-        justifyContent="space-evenly"
+        justifyContent="space-between"
+        p="10px 40px 10px 40px"
+        color={"#fff"}
+        bgGradient="linear(to-r, green.200, pink.500)"
       >
-        <h1>
-          <Link to="/">Dashboard</Link>
-        </h1>
-        <h1>
-          <Link to="/login">SignIn</Link>
-        </h1>
-        <h1>
-          <Link to="/signup">Signup</Link>
-        </h1>
+        <Box>
+          {" "}
+          <Text
+            color="black"
+            bgClip="text"
+            p="5px"
+            fontSize="3xl"
+            fontWeight="bold"
+            bgGradient="linear(to-l, #7928CA, #FF0080)"
+          >
+            <Link to="/">TaskManeger</Link>
+          </Text>
+        </Box>
+        <Box display="flex" gap={10}>
+          <h1>
+            <Link to="/login">SignIn</Link>
+          </h1>
+          <h1>
+            <Link to="/signup">Signup</Link>
+          </h1>
+        </Box>
       </Box>
       <Box>
         <Routes>
           <Route
             path="/"
             element={
-              <Privateroute>
-                <Dashboard />
-              </Privateroute>
+              // <Privateroute>
+              <Dashboard />
+              // </Privateroute>
             }
           />
           <Route path="/login" element={<Login />} />
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,7 +1,59 @@
+import { Box, Button, Heading, Image, Img } from "@chakra-ui/react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const dashboard = () => {
-  return <div>dashboard</div>;
-};
+function Dashboard() {
+  const navigate = useNavigate();
+  const handelClick = () => {
+    let token = localStorage.getItem("token");
+    if (!token) {
+      alert("please Login first");
+      navigate("/login");
+    } else {
+      navigate("/task");
+    }
+  };
 
-export default dashboard;
+  return (
+    <Box
+      h={["full", "full", "100vh"]}
+      bgGradient={[
+        "linear(to-tr, teal.300, yellow.400)",
+        "linear(to-t, blue.200, teal.500)",
+        "linear(to-b, orange.100, purple.300)",
+      ]}
+      display={"flex"}
+      alignItems="center"
+      flexDir={["column", "column", "row"]}
+    >
+      <Box w="50%" padding="10%">
+        {" "}
+        <Heading>
+          Task Manager allows administrators to terminate applications and
+          processes, adjust processing priorities and set processor affinity as
+          needed for best performance
+        </Heading>
+        <Button
+          onClick={handelClick}
+          w="70%"
+          bgGradient="linear(to-r, green.200, pink.500)"
+          color={"#fff"}
+          mt="30px"
+          fontWeight={"bold"}
+          fontSize="20px"
+          h="60px"
+        >
+          Set Up Tasks
+        </Button>
+      </Box>
+      <Box w="50%">
+        <Img
+          mt="9%"
+          src="https://images.ctfassets.net/rz1oowkt5gyp/3ZjLCD2fANfXYSN3ar9WpE/dc84a408c6a3ee89bee4a646ff6d5966/Lists_2x.png"
+        ></Img>
+      </Box>
+    </Box>
+  );
+}
+
+export default Dashboard;
